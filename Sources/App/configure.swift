47f15fd93cc0a446b3e7c52dd71149213ac7fb53
Brendan Brown <brendan@stickingpoint.net>
2024-01-25 20:56:50 -0700

    set up CORS

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,6 +5,20 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
+    // set up CORS
+    let corsConfiguration = CORSMiddleware.Configuration(
+    allowedOrigin: .all,
+    allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+    allowedHeaders: [
+        .accept, .authorization, .contentType, .origin, 
+        .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    // cors middleware should come before default error middleware using `at: .beginning`
+    app.middleware.use(cors, at: .beginning)
+
+
     // Configure server
     app.http.server.configuration.hostname = "0.0.0.0"
     app.http.server.configuration.port = 8080
